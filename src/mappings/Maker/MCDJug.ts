5dd94de19fa32b6c49ae98f46dfd6965941a730b
Caleb Sheridan <calebsheridan@gmail.com>
2020-05-02 19:22:58 +0200

    Add missing Maker params

--- src/mappings/Maker/MCDJug.ts
+++ src/mappings/Maker/MCDJug.ts
@@ -1,18 +1,26 @@
 import { log } from "@graphprotocol/graph-ts"
 import { LogNote } from '../../../generated/Maker_MCDJug/MCDJug'
+import { createParam, convertHexStringToBigIntString } from '../helpers'
 
 /** Handles anonymous event for `setDelay(address)`
  * 
  * @dev Maker docs https://docs.makerdao.com/smart-contract-modules/rates-module/jug-detailed-documentation
  */
 export function handleLogNoteSetDelay(event: LogNote): void {
-    // let debug_id = event.transaction.hash.toHexString()
-    let what = event.params.arg1.toString() // ??
-    let fax = event.params.arg2.toHexString() // ??
-    let usr = event.params.usr.toHexString()   // Spell contract address
-    // let sig = event.params.sig.toHexString()   // Signature // Lift = 0x3c278bd5
-    // let data = event.params.data.toHexString() // Value
+    let what = event.params.arg1.toString()
+    let fax = event.params.arg2.toHexString()
+    // let usr = event.params.usr.toHexString()   // ??
 
-    log.debug("MCDJug handleLogNoteSetDelay what: {} fax: {} usr: {}", [what, fax, usr])
-    log.warning("MCDJug what not handled: {}", [what])
+    if (what == "base") {
+        createParam(
+            "MCDJug-".concat(what),
+            "Fee applying to all collateral types",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else {
+        log.warning("MCDJug what not handled: {} fax {}", [what, fax])
+    }
 }
--- src/mappings/Maker/MCDVow.ts
+++ src/mappings/Maker/MCDVow.ts
@@ -1,17 +1,62 @@
 import { log } from "@graphprotocol/graph-ts"
 import { LogNote } from '../../../generated/Maker_MCDVow/MCDVow'
+import { createParam, convertHexStringToBigIntString } from '../helpers'
 
 /** Handles anonymous event for `setDelay(address)`
  * 
  * @dev Maker docs https://docs.makerdao.com/smart-contract-modules/system-stabilizer-module/vow-detailed-documentation
  */
 export function handleLogNoteSetDelay(event: LogNote): void {
-    // let debug_id = event.transaction.hash.toHexString()
-    let what = event.params.arg1.toString() // ??
-    let fax = event.params.arg2.toHexString() // ??
-    let usr = event.params.usr.toHexString()   // Spell contract address
+    let what = event.params.arg1.toString()
+    let fax = event.params.arg2.toHexString()
+    // let usr = event.params.usr.toHexString()   // ??
 
-    log.debug("MCDVow handleLogNoteSetDelay what: {} fax: {} usr: {}", [what, fax, usr])
-    
-    log.warning("MCDVow what not handled: {}", [what])
+    if (what == "wait") {
+        createParam(
+            "MCDVow-".concat(what),
+            "Length of the debt queue",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else if (what == "hump") {
+        createParam(
+            "MCDVow-".concat(what),
+            "Surplus buffer",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else if (what == "sump") {
+        createParam(
+            "MCDVow-".concat(what),
+            "Debt auction bid size",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else if (what == "dump") {
+        createParam(
+            "MCDVow-".concat(what),
+            "Debt auction lot size",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else if (what == "bump") {
+        createParam(
+            "MCDVow-".concat(what),
+            "Surplus auction lot size",
+            "Maker",
+            event.address.toHexString(),
+            convertHexStringToBigIntString(fax)
+        )
+    }
+    else {
+        log.warning("MCDVow what not handled: {} fax {}", [what, fax])
+    }
 }
